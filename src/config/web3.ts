fa22969082f7652c6a0eab278598c81cf1139a78
Anna <32278291+annasudol@users.noreply.github.com>
2024-12-23 19:39:10 +0100

    web conf

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -1,15 +1,18 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { http } from 'viem';
 import { arbitrum } from 'viem/chains';
 
 import { AppConfig } from './AppConfig';
 
 export const WALLETCONNECT_PROJECT_ID =
-  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
-  'a41cbf847cbe30877e9ef4aa8577d989';
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '';
 
 export const wagmiConfig = getDefaultConfig({
   appName: AppConfig.site_name,
   projectId: WALLETCONNECT_PROJECT_ID as string,
   chains: [arbitrum],
   ssr: true,
+  transports: {
+    [arbitrum.id]: http(process.env.NEXT_PUBLIC_ARBITRUM_RPC_URL),
+  },
 });
