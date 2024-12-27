bec5494f7322696c274b0e605263ce728446cc4f
Anna <32278291+annasudol@users.noreply.github.com>
2024-12-27 20:37:25 +0100

    update layout to ensure minimum height for content in modular index page

--- src/pages/modular/[address]/index.tsx
+++ src/pages/modular/[address]/index.tsx
@@ -7,7 +7,7 @@ import VaultProvider from '@/providers/VaultProvider';
 const Index = () => (
   <VaultProvider>
     <ValultLayout>
-      <div className="mx-auto mt-12 rounded-lg bg-white p-4 sm:max-w-lg">
+      <div className="mx-auto mt-12 min-h-60 rounded-lg bg-white p-4 sm:max-w-lg">
         <StepForm />
       </div>
     </ValultLayout>
