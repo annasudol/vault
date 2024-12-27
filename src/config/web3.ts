67bc30d06274202db9260b385c8c8a8c874b658f
Anna <32278291+annasudol@users.noreply.github.com>
2024-12-27 17:55:14 +0100

    fix build

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -5,8 +5,7 @@ import { arbitrum } from 'viem/chains';
 import { AppConfig } from './AppConfig';
 
 export const WALLETCONNECT_PROJECT_ID =
-  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
-  'a41cbf847cbe30877e9ef4aa8577d989';
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '';
 
 export const wagmiConfig = getDefaultConfig({
   appName: AppConfig.site_name,
