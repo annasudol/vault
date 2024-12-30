6259c8f90571db10cdc5bc66b0970523743d8312
Anna <32278291+annasudol@users.noreply.github.com>
2024-12-30 11:29:43 +0100

    adjust layout margins in MainHeader component for improved responsiveness

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -3,7 +3,7 @@ import { AppConfig } from '@/config/AppConfig';
 const MainHeader = () => (
   <header className="bg-slate-200">
     <div className="mx-auto my-12 max-w-screen-lg">
-      <div className="mb-12 max-w-screen-lg text-center">
+      <div className="mx-8 mb-12 text-center">
         <h1 className="text-center text-4xl font-bold">{AppConfig.title}</h1>
         <div className="mt-4 text-xl md:px-20">{AppConfig.description}</div>
       </div>
